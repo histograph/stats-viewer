51890d6bd80a1932cf11bba61b8a1c6c3ba94ee8
Bert Spaan <bert.spaan@gmail.com>
2015-08-24 18:22:34 +0200

    ugly kdsufhsdlifhli

--- js/stats.js
+++ js/stats.js
@@ -1,35 +1,58 @@
 var apiUrl = 'http://localhost:3001/';
 
+var hierarchy = [
+  'hg:Address',
+  'hg:Building',
+  'hg:Monument',
+  'hg:Fort',
+  'hg:Street',
+  'hg:Neighbourhood',
+  'hg:Borough',
+  'hg:Place',
+  'hg:Municipality',
+  'hg:Water',
+  'hg:Polder',
+  'hg:Area',
+  'hg:Region',
+  'hg:Baljuwschap',
+  'hg:Barony',
+  'hg:Countship',
+  'hg:Heerlijkheid',
+  'hg:Departement',
+  'hg:Province',
+  'hg:Country'
+];
+
 var queryResultsFormatters = {
-  'liesin-per-dataset': {
+  'http://localhost:3001/stats/queries/liesin-per-dataset': {
     title: 'hg:liesIn per dataset',
     format: function(e, json) {
     // title, description, format()
     }
   },
 
-  'pits-without-relations': {
+  'http://localhost:3001/stats/queries/pits-without-relations': {
     title: 'PITs without outgoing or incoming relations',
     format: function(e, json) {
 
     }
   },
 
-  'samehgconcept-between-datasets': {
+  'http://localhost:3001/stats/queries/samehgconcept-between-datasets': {
     title: 'hg:sameHgConcept relations between datasets',
     format: function(e, json) {
 
     }
   },
 
-  'types-per-dataset': {
+  'http://localhost:3001/stats/queries/types-per-dataset': {
     title: 'Types per dataset',
     format: function(e, json) {
-
+      console.log(json)
     }
   },
 
-  'vacant-pits': {
+  'http://localhost:3001/stats/queries/vacant-pits': {
     title: 'Vacant PITs',
     format: function(e, json) {
 
@@ -51,8 +74,8 @@ d3.json(apiUrl + 'stats/queries', function(json) {
 
           var container = e.append('div');
 
-          d3.json(apiUrl + 'stats/queries/' + d, function(json) {
-            queryResultsFormatter[d].format(container, json);
+          d3.json(d, function(json) {
+            queryResultsFormatters[d].format(container, json);
           });
         }
       });
